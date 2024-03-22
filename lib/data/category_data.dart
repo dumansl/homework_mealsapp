1d2ff1d08ba49c368e8e7a1571f49c331ac180be
Şule Duman <sduman_816@hotmail.com>
2024-03-22 17:14:30 +0300

    Initial commit

---
+++ lib/data/category_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:homework_mealsapp/models/category.dart';
+
+const categories = [
+  Category(id: "1", name: "Başlangıçlar", color: Colors.purple),
+  Category(id: "2", name: "Ara Sıcaklar", color: Colors.brown),
+  Category(id: "3", name: "Ana Yemekler", color: Colors.deepOrange),
+  Category(id: "4", name: "Aperatif", color: Colors.green),
+  Category(id: "5", name: "İcecekler", color: Colors.indigo),
+];
---
+++ lib/screens/categories.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:homework_mealsapp/data/category_data.dart';
+import 'package:homework_mealsapp/models/category.dart';
+import 'package:homework_mealsapp/widgets/category_card.dart';
+
+// Global State - Global State Management
+class Categories extends StatelessWidget {
+  const Categories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Category> categoryList = categories;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Kategoriler")),
+      // Inkwell, GestureDetector
+      body: GridView(
+        padding: const EdgeInsets.all(8),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 25,
+            mainAxisSpacing: 5,
+            childAspectRatio: 4 / 2),
+        children: categoryList
+            .map((e) => CategoryCard(
+                  color: e.color,
+                  name: e.name,
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
